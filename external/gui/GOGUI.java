bdc925b072729ec0e81d142ebe6f9f84cc20eee2
mvandermade <33425497+mvandermade@users.noreply.github.com>
2018-01-30 22:14:14 +0100

    Javadoc automatically generated documents

--- external/gui/GOGUI.java
+++ external/gui/GOGUI.java
@@ -16,8 +16,8 @@ public interface GOGUI {
     /**
      * Adds a new stone to the board of the given type and at the given position.
      * Removes any existing stone/area indicator at the given position.
-     * @param x the x coordinate of the new stone, ranges from 0 to boardSize - 1.
-     * @param y the y coordinate of the new stone, ranges from 0 to boardSize - 1.
+     * @param row the x coordinate of the new stone, ranges from 0 to boardSize - 1.
+     * @param col the y coordinate of the new stone, ranges from 0 to boardSize - 1.
      * @param white if true then a white stone will be added, otherwise a black stone will be added
      * @throws InvalidCoordinateException when x or y coordinate fall outside of the board.
      */
